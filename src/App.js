212471a7c2325cafa78c3687ccdf24992608f2eb
rhnchowdhury <rhnchdri@gmail.com>
2024-09-08 16:17:13 +0600

    some code add

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import './App.css';
-import { useEffect, useState } from 'react';
-import Countries from './Components/CountryData/Countries';
+import "./App.css";
+import { useEffect, useState } from "react";
+import Countries from "./Components/CountryData/Countries";
 
 function App() {
-
   const [allCountry, setAllCountry] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(true);
 
   useEffect(() => {
-    fetch('https://restcountries.com/v3.1/all')
-      .then(res => res.json())
-      .then(data => {
-        setAllCountry(data);
+    fetch("https://restcountries.com/v3.1/all")
+      .then((res) => {
+        if (!res.ok) {
+          throw Error("Failed to fetch");
+        } else {
+          return res.json();
+        }
       })
+      .then((data) => {
+        setAllCountry(data);
+        console.log(data);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        setError(err.message);
+        setIsLoading(false);
+      });
   }, []);
 
   return (
     <div>
       <h1>Total Country: {allCountry.length}</h1>
-      <div className='country'>
-        {
-          allCountry.map(country => <Countries country={country} key={country.cca2}></Countries>)
-        }
+      {isLoading && <p>Data is loading</p>}
+      {error && <p>{error}</p>}
+      <div className="country">
+        {allCountry.map((country) => (
+          <Countries country={country} key={country.cca2}></Countries>
+        ))}
       </div>
     </div>
   );
